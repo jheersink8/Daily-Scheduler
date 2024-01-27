b85b478c54a30d5fc8f2091544336826e87f66bd
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-01-26 22:43:06 -0700

    Present logic working. Doing past next.

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,13 @@
+var hour = dayjs().format("hA")
+console.log(hour);
+
+
+var unixT = dayjs().unix();
+console.log(unixT);
+
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -14,10 +24,27 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+  for (var i = 0; i < $(".hour").length; i++) {
+    var hourNum = "#hour-"+i;
+    if ($(hourNum+ " .hour").text() === hour) {
+      $(hourNum).attr("class", "row time-block present");
+    }
+  };
+
+
+
+
+
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var currentDay = $("#currentDay");
+  var today = dayjs().format("dddd, MMMM D");
+  currentDay.text(today);
 });
+
