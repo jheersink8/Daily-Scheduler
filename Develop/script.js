7304376a0c2fb77937b547c310d803032f012567
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-01-27 20:23:29 -0700

    Text content saving to local storage with appropriate ID. Feature complete.

--- Develop/script.js
+++ Develop/script.js
@@ -4,20 +4,15 @@
 
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  
-
   // -----------------------------SAVE BUTTON-----------------------------//
+  // Use "for each" with "this" to determine which queried button has been clicked.//
   var saveButtons = $(".saveBtn")
   saveButtons.each(function () {
     $(this).on("click", function () {
-      console.log($(this).closest("div").attr("id"));
-      console.log($(this).prev("textarea").val());
+      // Define variables for clicked "div ID" and "text content". Saves those values to local storage with the "div ID" (var = hourID) as the key and the "text content" (var = meetingText) as the saved value.//
+      var hourID = ($(this).closest("div").attr("id"));
+      var meetingText = ($(this).prev("textarea").val());
+      localStorage.setItem(hourID, meetingText);
     })
   });
 
